f6fd35a847e87e770ce61aa8707836d103911223
Chris Sullivan <csullivan@reliaslearning.com>
2018-09-26 15:19:31 -0400

    added yelp mediator service

---
+++ sullied-services/Models/Filters/YelpFilters.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace sullied_services.Models.Filters
+{
+    public class YelpFilters
+    {
+        public string Term { get; set; }
+        public string Location { get; set; }
+        public int Radius { get; set; }
+        public string Categories { get; set; }
+        public string Price { get; set; }
+        public int OpenAt { get; set; }
+    }
+}
